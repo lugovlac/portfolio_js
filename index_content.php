5254007bdc965436bf2b0eb86965b9d1256c42c1
Luciana Calvo <lugovlac@gmail.com>
2021-08-02 17:22:59 -0300

    blank

--- index_content.php
+++ index_content.php
@@ -2,7 +2,7 @@
     <div class="row justify-content-center">
         <div class="col-12 col-lg-8">
             <h2 class="text-center mb-3">Hola!</h2>
-            <p class="mb-4 text-center pHeading">Este proyecto es una reversión de unos ejercicios que hice para la facultad hace varios años. En esta ocasión utilicé javascript vanilla, sin librerías (aunque utilizo bootstrap para styling e interacción básica y algunos plugins sin dependencias), y apliqué el paradigma OOP. Además intenté modernizar el javascript para que esté más acorde a las últimas especificaciones ES. El primer ejercicio es <a href="fernet.php">una calculadora para preparar fernet</a>, y el segundo es un juego de dados que se juega contra la computadora: <a href="juego.php">el Chanchito</a>. El codebase de este proyecto lo pueden encontrar en <a href="https://github.com/lugovlac/portfolio_js">el repositorio de github</a>.</p>
+            <p class="mb-4 text-center pHeading">Este proyecto es una reversión de unos ejercicios que hice para la facultad hace varios años. En esta ocasión utilicé javascript vanilla, sin librerías (aunque utilizo bootstrap para styling e interacción básica y algunos plugins sin dependencias), y apliqué el paradigma OOP. Además intenté modernizar el javascript para que esté más acorde a las últimas especificaciones ES. El primer ejercicio es <a href="fernet.php">una calculadora para preparar fernet</a>, y el segundo es un juego de dados que se juega contra la computadora: <a href="juego.php">el Chanchito</a>. El codebase de este proyecto lo pueden encontrar en <a href="https://github.com/lugovlac/portfolio_js" target="_blank">el repositorio de github</a>.</p>
             <p class="text-center pHeading">Gracias por la visita! :)</p>
         </div>
     </div>
